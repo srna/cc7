eecb744066bfb2fcc57261b5443aab8036d2f6ec
Petr Dvorak <petr@inmite.eu>
2016-08-15 19:57:48 +0200

    Fix script to redirect both stdout and errout to /dev/null

--- sync_branches.sh
+++ sync_branches.sh
@@ -4,7 +4,7 @@
 # - https://help.github.com/articles/configuring-a-remote-for-a-fork/
 # - https://help.github.com/articles/syncing-a-fork/"
 
-git ls-remote --exit-code upstream > /dev/null
+git ls-remote --exit-code upstream &> /dev/null
 if test $? != 0; then
     git remote add upstream https://github.com/hvge/cc7.git
 fi
