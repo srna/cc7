245981533f3f4c9db33c69137a4aa5dc206253ac
Petr Dvorak <petr@inmite.eu>
2016-08-15 17:10:31 +0200

    Add readme header

--- sync_branches.sh
+++ sync_branches.sh
@@ -1,3 +1,5 @@
+# First, add upstream branch:
+# $ git remote add upstream https://github.com/hvge/cc7.git
 # See following articles for detail:
 # - https://help.github.com/articles/configuring-a-remote-for-a-fork/
 # - https://help.github.com/articles/syncing-a-fork/"
