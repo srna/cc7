9c102890ac5bb7d5bea67364413f34ac03c8fec8
Petr Dvorak <petr@inmite.eu>
2016-08-15 19:52:19 +0200

    Fix sync_branches.sh script

--- sync_branches.sh
+++ sync_branches.sh
@@ -4,6 +4,11 @@
 # - https://help.github.com/articles/configuring-a-remote-for-a-fork/
 # - https://help.github.com/articles/syncing-a-fork/"
 
+git ls-remote --exit-code upstream > /dev/null
+if test $? != 0; then
+    git remote add upstream https://github.com/hvge/cc7.git
+fi
+
 git fetch upstream
 git checkout master
 git merge upstream/master
